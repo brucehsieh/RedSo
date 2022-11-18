7498fe55702ee3255c32905173ad0005978671cc
brucehsieh <cjhsieh717@gmail.com>
2022-11-18 13:09:24 +0800

    Initial Commit

---
+++ RedSo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RedSo
+//
+//  Created by Bruce Hsieh on 2022/11/18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
